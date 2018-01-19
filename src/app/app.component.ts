6d90d82559282c0834280217e7fee5e51ab626a3
15138730370 <1037483576@qq.com>
2018-01-20 07:56:19 +0800

    documention

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,14 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import { BarService } from '@my/lib';
 
 @Component({
   selector: 'app-root',
-  template: `
-<my-foo></my-foo>
-<hr>
-<marquee>{{ value$ | async }}</marquee>
-`,
+  template: ``,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
 
-  value$: Observable<string>;
-
-  constructor (
-    bar: BarService
-  ) {
-     this.value$ = bar.value;
+  constructor () {
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { MyLibModule } from '@my/lib';
 
 @NgModule({
   declarations: [
@@ -13,8 +12,7 @@ import { MyLibModule } from '@my/lib';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
-    MyLibModule.forRoot()
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
